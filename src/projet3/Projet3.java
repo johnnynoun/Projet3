c1abbb430976ea25ec3ae00f04a301f5933e5e8c
U s e r <U s e r@DESKTOP-PUGJ7NO>
2020-02-01 18:31:13 +0200

    Projet3

--- src/projet3/Projet3.java
+++ src/projet3/Projet3.java
@@ -5,7 +5,8 @@ package projet3;
 public class Projet3 {
 
     public static void main(String[] args) {
-        System.out.println("johnny");
+      
+         mainmenu mainmenu1= new mainmenu();
     }
     
 }
